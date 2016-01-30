8f089d69edb2c803a67be79e9a78526cca344910
Mike de Heij <mike@deheij.email>
2016-01-31 00:45:53 +0100

    Add action handling and test file

---
+++ services/action.go
@@ -0,0 +1,62 @@
+package services
+
+import (
+	"fmt"
+	"git.gate.sh/mdeheij/monitoring/services/handlers"
+	"strconv"
+	"time"
+)
+
+type ActionConfig struct {
+	Name           string
+	Telegramtarget []int32
+	Rpecommand     string
+}
+
+type ActionHandler struct {
+	name    string
+	service Service
+}
+
+func NewAction(service Service) *ActionHandler {
+	var a ActionHandler = ActionHandler{name: "telegram"}
+	a.service = service
+	return &a
+}
+
+func (a ActionHandler) buildMessage() (msg string) {
+	timestamp := a.service.LastCheck.Format(time.Stamp)
+
+	thresholdCounting := strconv.Itoa(a.service.ThresholdCounter) + "/" + strconv.Itoa(a.service.Threshold)
+
+	actionTypeString := ""
+	switch a.service.Health {
+	case 2:
+		actionTypeString = "🔴 PROBLEM"
+	case 0:
+		actionTypeString = "🔵 RECOVERY"
+	case 1:
+		actionTypeString = "⚠️ WARNING"
+	}
+
+	return "*" + actionTypeString + " - " + timestamp + "*\n" + a.service.Identifier + " (" + a.service.Host + ")" + "\nThreshold: " + thresholdCounting + "\nOutput: _" + a.service.Output + "_"
+}
+
+func (a ActionHandler) Run() {
+	if SilenceAll == false {
+		if a.service.Acknowledged != true {
+
+			switch a.service.Action.Name {
+			case "telegram":
+				handlers.Telegram(a.service.Action.Telegramtarget, a.buildMessage())
+			case "rpe":
+				handlers.RemotePluginExecutor(a.service.Host)
+			default:
+				handlers.Telegram(a.service.Action.Telegramtarget, a.buildMessage())
+			}
+
+		}
+	} else {
+		fmt.Println("Silenced.")
+	}
+}
---
+++ services/action_test.go
@@ -0,0 +1,38 @@
+package services
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+)
+
+var target int
+
+func init() {
+	flag.IntVar(&target, "target", 0, "The telegram target")
+	flag.Parse()
+}
+
+func Test(t *testing.T) {
+	//fmt.Println(ServicesConfig)
+	fmt.Println("[Testing]")
+
+	if target == 0 {
+		fmt.Println("Telgeram Target ID is required. Use --target CHAT_ID")
+		os.Exit(2)
+	}
+	//tgtg := []int32{4009810, -62946040}
+	cijfer := int32(target)
+	tgtg := []int32{cijfer}
+	ac := ActionConfig{Name: "telegram", Telegramtarget: tgtg}
+	s := Service{Host: "localhost", Identifier: "test", Threshold: 3, Health: 1, Output: "OK", Action: ac}
+
+	a := NewAction(s)
+	a.Run()
+
+	s = Service{Host: "dev", Identifier: "localhost", Threshold: 3, Health: 1, Output: "OK", Action: ActionConfig{Name: "rpe"}}
+
+	a = NewAction(s)
+	a.Run()
+}
