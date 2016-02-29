7f2490a451b47cf58c903fd1b62d525e33c3ce1e
Mike de Heij <mike@deheij.email>
2016-02-29 23:13:51 +0100

    Add future testing flag for config and services

--- main.go
+++ main.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/mdeheij/monitoring/configuration"
 	"github.com/mdeheij/monitoring/server"
+	"os"
 )
 
 var debug bool
-var autostart bool
+var testconfig bool
 var config string
 
 func init() {
 
 	flag.BoolVar(&debug, "debug", false, "Enable debugging")
-	flag.BoolVar(&autostart, "autostart", true, "Start checking automatically")
+	flag.BoolVar(&testconfig, "test", false, "Test configuration instead of running")
 	flag.StringVar(&config, "config", "/etc/monitoring/config.json", "Path to config file")
 	flag.Parse()
 }
 
 func main() {
+
+	if testconfig == true {
+		fmt.Println("TODO: implement this")
+		os.Exit(2)
+	}
+
 	configuration.Init(config)
-	server.Setup(debug, autostart)
+	server.Setup(debug, true)
 }
