2f5bc47c87b9991a3962f3d6a90bbadc00b06b87
Mike de Heij <mike@deheij.email>
2016-02-29 17:10:30 +0100

    Add autostart boolean to prevent automatic checking on startup

--- main.go
+++ main.go
@@ -7,16 +7,18 @@ import (
 )
 
 var debug bool
+var autostart bool
 var config string
 
 func init() {
 
 	flag.BoolVar(&debug, "debug", false, "Enable debugging")
+	flag.BoolVar(&autostart, "autostart", true, "Start checking automatically")
 	flag.StringVar(&config, "config", "/etc/monitoring/config.json", "Path to config file")
 	flag.Parse()
 }
 
 func main() {
 	configuration.Init(config)
-	server.Setup(debug)
+	server.Setup(debug, autostart)
 }
