7aa59232e66a1c2cd1d77ef80bc50bb329519a07
Mike de Heij <mike@deheij.email>
2016-03-20 15:28:52 +0100

    Server publicgroup support

--- server/service.go
+++ server/service.go
@@ -2,18 +2,21 @@ package server
 
 import (
 	"encoding/json"
+
 	"github.com/gin-gonic/gin"
-	"github.com/mdeheij/gin-csrf"
-	"github.com/mdeheij/monitoring/services"
+	//"github.com/mdeheij/gin-csrf"
 	"strconv"
 	"strings"
+
+	"github.com/mdeheij/gin-csrf"
+	"github.com/mdeheij/monitoring/services"
 )
 
 func servicesInit(r *gin.Engine, debug bool, autostart bool) {
-	if debug {
+	if debug == true {
 		services.EnableDebug()
 	}
-	if autostart {
+	if autostart == true {
 		services.Start()
 	}
 
@@ -37,15 +40,15 @@ func servicesInit(r *gin.Engine, debug bool, autostart bool) {
 		monitoringGroup.GET("/list", servicesGetServicesAsJSON)
 		monitoringGroup.GET("/list/:identifier", servicesGetServicesWithIdentifier)
 	}
-	embedGroup := r.Group("/embed")
+	publicGroup := r.Group("/public")
 	{
-		embedGroup.GET("/", servicesGetServicesEmbed)
+		//embedGroup.GET("/embed", servicesGetServicesEmbed)
+		publicGroup.GET("/status/:group", servicesGetPublicServices)
 	}
 }
 
 func servicesStart(c *gin.Context) {
 	services.Start()
-
 	c.JSON(200, gin.H{
 		"task":   "start",
 		"status": services.DaemonActive,
@@ -54,7 +57,6 @@ func servicesStart(c *gin.Context) {
 
 func servicesUpdateList(c *gin.Context) {
 	services.UpdateList()
-
 	c.JSON(200, gin.H{
 		"task": "updateList",
 	})
@@ -81,24 +83,21 @@ func servicesUpdate(c *gin.Context) {
 	var result string
 	var lastCheckOld string
 	var lastCheckNew string
-
 	if identifier != "" {
 		service, _ := services.Services.Get(identifier)
 		lastCheckOld = service.LastCheck.String()
 		//lastCheckOld = services.Services[identifier].LastCheck.String()
-		lastCheckNew = service.LastCheck.String()
 		result = service.Update()
+		lastCheckNew = service.LastCheck.String()
 	} else {
 		result = "No parameter to update!"
 	}
-
 	c.JSON(200, gin.H{
 		"result":       result,
 		"lastCheckOld": lastCheckOld,
 		"lastCheckNew": lastCheckNew,
 	})
 }
-
 func servicesStop(c *gin.Context) {
 	services.Stop()
 	c.JSON(200, gin.H{
@@ -107,7 +106,6 @@ func servicesStop(c *gin.Context) {
 	})
 
 }
-
 func servicesGetServicesAsJSON(c *gin.Context) {
 	result, _ := json.Marshal(services.Services)
 	c.String(200, string(result))
@@ -115,12 +113,14 @@ func servicesGetServicesAsJSON(c *gin.Context) {
 
 //servicesGetServicesWithIdentifier returns the services which match the given identifier as a json
 func servicesGetServicesWithIdentifier(c *gin.Context) {
+
 	identifier := c.Param("identifier")
 
 	if identifier != "" {
 		var s []services.Service
 
 		for item := range services.Services.Iter() {
+
 			service := item.Val
 
 			service.Host = strings.Replace(service.Host, "http://", "", -1)
@@ -132,16 +132,20 @@ func servicesGetServicesWithIdentifier(c *gin.Context) {
 				s = append(s, service)
 			}
 		}
-
 		result, _ := json.Marshal(s)
-
 		if len(s) > 0 {
 			c.String(200, string(result))
 			return
 		}
 	}
-
 	c.JSON(200, gin.H{})
+}
+
+//PublicService is a stripped down struct which can be used for public status pages
+type PublicService struct {
+	Identifier  string
+	Description string
+	Online      bool
 }
 
 func getProblematicServices() []services.Service {
@@ -154,12 +158,28 @@ func getProblematicServices() []services.Service {
 			s = append(s, service)
 		}
 	}
-
 	return s
+}
+
+func getPublicServices(group string) []PublicService {
+
+	var publicServices []PublicService
+
+	for item := range services.Services.IterBuffered() {
+		originalService := item.Val
+
+		publicService := PublicService{Identifier: originalService.Identifier, Description: originalService.Description}
+		if originalService.Health > 0 {
+			publicService.Online = true
+		}
+		publicServices = append(publicServices, publicService)
+	}
+	return publicServices
 }
 
 //servicesGetServicesEmbed returns the services as a nice embed for Grafana
 func servicesGetServicesEmbed(c *gin.Context) {
+
 	//hostname := c.Param("hostname")
 
 	c.HTML(200, "embed.html", gin.H{
@@ -170,4 +190,16 @@ func servicesGetServicesEmbed(c *gin.Context) {
 	})
 	//TODO: think how I'm going to build this
 	//	c.JSON(200, gin.H{})
+}
+
+//servicesGetServicesEmbed returns the services as a nice embed for Grafana
+func servicesGetPublicServices(c *gin.Context) {
+
+	group := c.Param("group")
+
+	//test, _ := services.Services.Get("company.dns.ns1")
+
+	c.JSON(200, gin.H{
+		"Zooi": getPublicServices(group),
+	})
 }
