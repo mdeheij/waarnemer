a2fdbb0accdfbb8054b340c77b5784a268b8d272
Mike de Heij <mike@deheij.email>
2016-10-09 16:51:39 +0200

    Add ./vendor/services/handlers/telegram.go

---
+++ vendor/services/handlers/telegram.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"configuration"
+	"log"
+
+	"github.com/bartholdbos/golegram"
+)
+
+var bot *golegram.Bot
+
+func SetupTelegram() {
+	var err error
+	log.Error("Instance bot initialized! Using token:", configuration.Config.TelegramBotToken)
+	bot, err = golegram.NewBot(configuration.Config.TelegramBotToken)
+	if err != nil {
+		log.Error("[Telegram] Init error")
+		log.Error(err)
+	}
+}
+
+func checkMessageError(result golegram.Message, err error) {
+	if err != nil {
+		log.Error("[Telegram] Message error")
+		log.Error(err)
+		log.Error(result)
+	}
+}
+
+//Telegram sends a Telegram message to one or more users by their unique ID
+func Telegram(targets []string, message string) {
+	var err error
+
+	//Check if a bot instance have been constructed before
+	if bot == nil {
+		SetupTelegram()
+	}
+
+	if err == nil {
+
+		if len(targets) >= 1 {
+			//If targets have been set up in the specific service, use them
+			for _, target := range targets {
+				//disable_web_page_preview bool, parse_mode string
+				result, err := bot.SendMessage(target, message, true, "Markdown")
+				checkMessageError(result, err)
+			}
+		} else {
+			//If not, then use default one
+			target := configuration.Config.TelegramNotificationTarget
+			result, err := bot.SendMessage(target, message, true, "Markdown")
+			checkMessageError(result, err)
+		}
+
+	} else {
+		log.Error("Error sending Telegram message!", err.Error())
+	}
+}
