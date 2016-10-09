d9cfbb8fd645d8be5e418721a0a219cbd73a9942
Mike de Heij <mike@deheij.email>
2016-10-09 16:51:39 +0200

    Add ./vendor/services/handlers/remotepluginexecutor.go

---
+++ vendor/services/handlers/remotepluginexecutor.go
@@ -0,0 +1,10 @@
+package handlers
+
+import (
+	"fmt"
+)
+
+//TODO: build this.
+func RemotePluginExecutor(message string) {
+	fmt.Println("[RemotePluginExecutor] Not implemented yet! Dropping: ", message)
+}
