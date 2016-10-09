f40a4f6de125ecc49388645f63865050867c9f15
Mike de Heij <mike@deheij.email>
2016-10-09 16:51:39 +0200

    Add ./vendor/api/main.go

---
+++ vendor/api/main.go
@@ -0,0 +1,79 @@
+package api
+
+import (
+	"configuration"
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	cors "github.com/itsjamie/gin-cors"
+)
+
+//AuthRequired is authentication middleware for user authenticaton.
+func AuthRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var err error
+		if err == nil {
+			c.Header("Content-Type", "application/json; charset=utf8") //TODO: This should be a seperate middleware
+			c.Next()
+		} else {
+			c.String(401, "Unauthorized.")
+			c.Abort()
+		}
+	}
+}
+
+func main() {
+	configuration.Init("../config.json") //TODO: fix this
+	Setup()
+}
+
+//Setup initializes routers, login and services.
+func Setup() {
+
+	if configuration.Debug {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	r := gin.New()
+	// If configuration values are empty, Gin will listen and serve on 0.0.0.0:8080.
+
+	r.Use(cors.Middleware(cors.Config{
+		Origins:         "*",
+		Methods:         "GET, PUT, POST, DELETE",
+		RequestHeaders:  "Origin, Authorization, Content-Type",
+		ExposedHeaders:  "",
+		MaxAge:          50 * time.Second,
+		Credentials:     true,
+		ValidateHeaders: false,
+	}))
+
+	servicesGroup := r.Group("/api/service/", AuthRequired())
+	{
+		// monitoringGroup.GET("/", servicesPage)
+		servicesGroup.POST("/start", servicesStart)
+		servicesGroup.POST("/stop", servicesStop)
+		servicesGroup.POST("/updatelist", servicesUpdateList)
+		servicesGroup.POST("/update/:identifier", servicesUpdate)
+		servicesGroup.POST("/reschedule/:identifier", servicesRescheduleCheck)
+		servicesGroup.OPTIONS("/list", servicesGetServicesAsJSON)
+		servicesGroup.GET("/list", servicesGetServicesAsJSON)
+		servicesGroup.GET("/list/:identifier", servicesGetServicesWithIdentifier)
+	}
+
+	publicGroup := r.Group("/public")
+	{
+		publicGroup.GET("/status/:group", servicesGetPublicServices)
+	}
+	log.Notice("Starting webserver")
+
+	r.Run("0.0.0.0:8080")
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
