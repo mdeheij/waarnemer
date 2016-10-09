e8df1762b710c98a1811bf652b775bd9dcb7c3ad
Mike de Heij <mike@deheij.email>
2016-10-09 16:51:39 +0200

    Add ./main.go

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"api"
+	"configuration"
+	"flag"
+	"log"
+	"os"
+	"services"
+)
+
+var debug bool
+var testConfig bool
+var apiEnabled bool
+var autoStart bool
+var noActionHandling string
+var config string
+
+func init() {
+	log.Notice("Initializing..")
+	flag.BoolVar(&debug, "debug", false, "Enable debugging")
+	flag.BoolVar(&testConfig, "test", false, "Test configuration instead of running")
+	flag.BoolVar(&apiEnabled, "api", true, "Enable Web API")
+	flag.BoolVar(&autoStart, "autostart", true, "Autostart service checking")
+	flag.BoolVar(&noActionHandling, "no-action-handling", false, "Do not do anything when services are down")
+	flag.StringVar(&config, "config", "/etc/monitoring/config.json", "Path to config file")
+	flag.Parse()
+	configuration.Init(config)
+}
+
+func main() {
+	configuration.Init(config)
+	configuration.Debug = debug
+
+	if testConfig {
+		err := services.TestConfiguration()
+		if err != nil {
+			log.Panic(err)
+		} else {
+			os.Exit(0)
+		}
+	}
+
+	if noActionHandling {
+		configuration.Config.NoActionHandling = true
+	}
+
+	services.Init()
+
+	if autoStart {
+		services.Start()
+	}
+
+	api.Setup()
+}
